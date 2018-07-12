b65931487f7540d1d15f7e1e70c255a89f837a8e
Vanessa Pacheco <nesspachecod@gmail.com>
2018-07-12 17:18:23 -0500

    Add expect to setupTests file

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,8 +1,9 @@
 import Adapter from 'enzyme-adapter-react-16';
-import chai from 'chai';
+import chai, { expect } from 'chai';
 import chaiEnzyme from 'chai-enzyme';
 import { configure } from 'enzyme';
 
 configure({ adapter: new Adapter() });
 
 chai.use(chaiEnzyme());
+global.expect = expect;
